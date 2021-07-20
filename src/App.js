68f1e1039f285b16b07e8652e875772a91b188a2
Marc Stanley <marcson909@gmail.com>
2021-07-20 13:16:53 -0500

    complete whack-a-mole

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ class App extends Component {
     dens: this.getDensState(),
     points: 0,
   }
+
   componentDidMount() {
     this.startGame()
   }
+  
+  /**
+   * runs when page loads (using componentDidMount)
+   * Then, sets the den state using 'getDenState()' every 1500ms (1.5s)
+   * This is where the board gets refreshed, and game can be tuned using the interval.
+   */
   startGame() {
     setInterval(() => {
       this.setState({
@@ -17,6 +24,11 @@ class App extends Component {
       })
     }, 1500)
   }
+
+  /**
+   * Generates a 9 item array of {isMoleVisible:True/False} values using Math.random
+   * Each item of the array can be accessed by it's .isMoleVisible value (true/false)
+   */
   getDensState() {
     return new Array(9).fill({}).map(() => {
       return { 
@@ -24,17 +36,25 @@ class App extends Component {
       }
     })
   }
-  onMoleWhacked() {
+
+  // Have to rewrite this to fat arrow style to use 'this'
+  // Otherwise, have to bind 'this' in the mole.js component class
+  onMoleWhacked = () => {
     this.setState({
       points: this.state.points + 1
     })
   }
+
   render() {
     const dens = this.state.dens.map((den, index) => {
       return (
-        <Mole key={`mole-${index}`} />
+        <Mole key={`mole-${index}`} 
+        isMoleVisible={this.state.dens[index].isMoleVisible} 
+        onMoleWhacked={this.onMoleWhacked}
+        />
       )
     })
+
     return (
       <div className="App">
         <h1>WHACK-A-MOLE!</h1>
--- src/components/mole/Mole.js
+++ src/components/mole/Mole.js
@@ -4,9 +4,16 @@ import MoleIcon from './Mole.svg'
 
 class Mole extends Component {
   render() {
+
+    const {isMoleVisible, onMoleWhacked} = this.props
+
     return (
-      <div className="den">
-        <img src={MoleIcon} className="Mole" alt="Mole" />
+      <div  className="den">
+      {isMoleVisible &&
+        <img src={MoleIcon} className="Mole" 
+        onClick={onMoleWhacked}
+        alt="Mole" />
+        }
       </div>
     )
   }
